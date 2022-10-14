17283155da563ae90464afbbda25141644bafbda
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-10-14 22:41:38 +0100

    created plans section

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Plans.css';
+import { plansData } from '../../data/plansData';
+import WhiteTick from '../../assets/whiteTick.png';
+
+function Plans() {
+  return (
+    <div className="plans-container">
+      <div className="programs-header" STYLE={{ gap: '2rem' }}>
+        <span className="stroke-text">READY TO START</span>
+        <span>YOUR JOURNEY</span>
+        <span className="stroke-text">NOW WITHUS</span>
+      </div>
+
+      {/* plans card */}
+      <div className="plans">
+        {plansData.map((plan, i) => (
+          <div className="plan" key={i}>
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>$ {plan.price}</span>
+
+            <div className="features">
+              {plan.features.map((feature, i) => (
+                <div className="feature">
+                  <img src={WhiteTick} alt="" />
+                  <span key={i}>{feature}</span>
+                </div>
+              ))}
+            </div>
+
+            <div>
+              <span>See more benefits</span>
+            </div>
+            <button className="btn">Join Now</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Plans;
