674c7956f64c6ea8556d6852aadf4f95eb0318b4
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-10-16 09:36:57 +0100

    created blur effect

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,6 +9,7 @@ import Calories from '../../assets/calories.png';
 function Hero() {
   return (
     <div className="hero">
+      <div className="blur hero-blur"></div>
       <div className="left-h">
         <Header />
 
